ac59913148b84c9d548f1bca39af440548a4e1c5
Edvinas <edvinasalimas98@gmail.com>
2022-06-29 21:46:30 +0300

    Update CartController

--- src/main/java/com/data/company/shop/cart/controller/CartController.java
+++ src/main/java/com/data/company/shop/cart/controller/CartController.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +23,7 @@ public class CartController {
   private final CartCommandService commandService;
   private final CartQueryService queryService;
 
-  @PostMapping
+  @PutMapping
   public void createCart(Cart cart) {
     commandService.createCart(cart);
   }
