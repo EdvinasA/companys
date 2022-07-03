179692e3891d8e1d992b726c857e243b2cbc89e0
Edvinas <edvinasalimas98@gmail.com>
2022-07-03 10:29:33 +0300

    SHOP-20 Add test

--- src/integrationTest/java/com/data/company/shop/laptops/controller/LaptopControllerIntegrationTest.java
+++ src/integrationTest/java/com/data/company/shop/laptops/controller/LaptopControllerIntegrationTest.java
@@ -1,10 +1,29 @@
 package com.data.company.shop.laptops.controller;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.data.company.shop.base.MvcTest;
 import com.data.company.shop.extensions.WiremockExtension;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @ExtendWith(WiremockExtension.class)
 public class LaptopControllerIntegrationTest extends MvcTest {
 
+  @Test
+  @SneakyThrows
+  void getPageListOfLaptopsShouldReturnEmptyPageWhenNoLaptopExist() {
+    mvc.perform(MockMvcRequestBuilders
+    .get("/shop/laptop")
+    .contentType(MediaType.APPLICATION_JSON)
+    .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect((ResultMatcher) jsonPath("$.pageable.pageSize", is(10)));
+  }
 }
