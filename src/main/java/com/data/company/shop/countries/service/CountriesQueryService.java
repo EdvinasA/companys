684fa3c30d28f7c7318184f8fb5a136b1ec5c027
Edvinas <edvinasalimas98@gmail.com>
2022-08-03 21:25:27 +0300

    SHOP-45 Update country service get sorted list of countries

--- src/main/java/com/data/company/shop/countries/service/CountriesQueryService.java
+++ src/main/java/com/data/company/shop/countries/service/CountriesQueryService.java
@@ -2,9 +2,10 @@ package com.data.company.shop.countries.service;
 
 import com.data.company.shop.countries.model.CountryName;
 import com.data.company.shop.countries.repository.CountriesQueryRepository;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +15,10 @@ public class CountriesQueryService {
   private final CountriesQueryRepository queryRepository;
 
   public List<CountryName> getAll() {
-    return queryRepository.getAll();
+    return queryRepository.getAll()
+        .stream()
+        .sorted(Comparator.comparing(CountryName::getCommon))
+        .collect(Collectors.toList());
   }
 
 }
