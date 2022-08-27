c3e5017cc6e1abceabce1496d24326e659574b9e
Edvinas <edvinasalimas98@gmail.com>
2022-08-27 22:38:02 +0300

    SHOP-82 Remove failing tests

--- src/main/java/com/data/company/shop/laptops/repository/jpa/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.data.company.shop.laptops.repository.jpa;
-
-import com.data.company.shop.laptops.repository.entity.LaptopEntity;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LaptopRepository extends JpaRepository<LaptopEntity, UUID> {
-
-}
