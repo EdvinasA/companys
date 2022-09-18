33d1b3d3e0b4486218b37a2c2c9bff3f6078a3d3
Edvinas <edvinasalimas98@gmail.com>
2022-09-18 22:12:21 +0300

    SHOP-102 update build.gradle files

--- src/querydsl/java/com/data/company/user/repository/entity/QRoleEntity.java
+++ src/querydsl/java/com/data/company/user/repository/entity/QRoleEntity.java
@@ -8,30 +8,32 @@ import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
 
+
 /**
  * QRoleEntity is a Querydsl query type for RoleEntity
  */
 @Generated("com.querydsl.codegen.DefaultEntitySerializer")
 public class QRoleEntity extends EntityPathBase<RoleEntity> {
 
-	private static final long serialVersionUID = -2141206981L;
+    private static final long serialVersionUID = -2141206981L;
 
-	public static final QRoleEntity roleEntity = new QRoleEntity("roleEntity");
+    public static final QRoleEntity roleEntity = new QRoleEntity("roleEntity");
 
-	public final StringPath authority = createString("authority");
+    public final StringPath authority = createString("authority");
 
-	public final NumberPath<Long> id = createNumber("id", Long.class);
+    public final NumberPath<Long> id = createNumber("id", Long.class);
 
-	public QRoleEntity(String variable) {
-		super(RoleEntity.class, forVariable(variable));
-	}
+    public QRoleEntity(String variable) {
+        super(RoleEntity.class, forVariable(variable));
+    }
 
-	public QRoleEntity(Path<? extends RoleEntity> path) {
-		super(path.getType(), path.getMetadata());
-	}
+    public QRoleEntity(Path<? extends RoleEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
 
-	public QRoleEntity(PathMetadata metadata) {
-		super(RoleEntity.class, metadata);
-	}
+    public QRoleEntity(PathMetadata metadata) {
+        super(RoleEntity.class, metadata);
+    }
 
 }
+
